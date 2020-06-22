8a09c9eadb6e1ba405242f460524688ff6b41768
sunny-aguilar <sunny.aguilar@gmail.com>
2020-06-21 17:17:13 -0700

    jQuery practice

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,16 +3,28 @@
 
 $(function() {
 
+
+
+    /*
+        This selects the li elements on the list to make them
+        grey and cross them out. It also undoes these actions.
+    */
     $('li').click(function() {
         // turn black if gray
         if ($(this).css('color') === 'rgb(128, 128, 128)') {
-
+            $(this).css({
+                'color': 'black',
+                'text-decoration': 'none'
+            });
         }
         // turn grey if black
-        $(this).css({
-            'color': 'gray',
-            'text-decoration': 'line-through'
-        });
+        else {
+            $(this).css({
+                'color': 'gray',
+                'text-decoration': 'line-through'
+            });
+        }
+
     });
 
 
